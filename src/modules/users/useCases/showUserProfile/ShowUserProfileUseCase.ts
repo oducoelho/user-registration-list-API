1b8abefabd9126502fbbe307e0ab287af9654322
Eduardo Coelho <eduardocoelho619@gmail.com>
2022-12-14 19:34:08 -0300

    ending a search part by id

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const findUserById = this.usersRepository.findById(user_id);
+
+    if (!findUserById) {
+      throw new Error("Mensagem do erro");
+    }
+
+    return findUserById;
   }
 }
 
